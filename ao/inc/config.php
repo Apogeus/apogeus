90b392a436a178e755da0bfb38209110f984d1f2
Angel Guzman Maeso <shakaran@gmail.com>
2008-07-28 00:09:48 +0200

    Include config.php file

---
+++ ao/inc/config.php
@@ -0,0 +1,7 @@
+<?
+# Data config for connect to database server
+$server='localhost'; # Server (default:localhost)
+$username_mysql=''; # Mysql database user
+$password_mysql=''; # Mysql database password
+$database_mysql=''; # Mysql database
+?>
