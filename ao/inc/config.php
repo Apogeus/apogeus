01b41244a75fedd31f50de7ba68ab889628e8f83
Angel Guzman Maeso <shakaran@gmail.com>
2008-07-28 00:10:35 +0200

    Add tab config.php

--- ao/inc/config.php
+++ ao/inc/config.php
@@ -1,7 +1,7 @@
 <?
 # Data config for connect to database server
-$server='localhost'; # Server (default:localhost)
-$username_mysql=''; # Mysql database user
-$password_mysql=''; # Mysql database password
-$database_mysql=''; # Mysql database
+$server='localhost';    # Server (default:localhost)
+$username_mysql='';     # Mysql database user
+$password_mysql='';     # Mysql database password
+$database_mysql='';     # Mysql database
 ?>
